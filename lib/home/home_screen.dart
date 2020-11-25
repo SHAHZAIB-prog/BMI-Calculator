7a2a2aa93ec6c2e14681c46fc49e90022155f1d9
Shahzaib Ali Shah <60398811+SHAHZAIB-prog@users.noreply.github.com>
2020-11-25 18:04:54 +0500

    Adding scroll-view to manage screen BMI Calculator

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -69,186 +69,192 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     Widget _buildBodyWidget() {
-      return Container(
-        decoration: kMainContainerDecoration,
-        height: MediaQuery.of(context).size.height < 632.0
-            ? 632.0
-            : double.infinity,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width / 12.0,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 20.0,
-                      ),
-                      child: Text(
-                        MyLocalizations.of(context).gender,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20
+      return SingleChildScrollView(
+        child:Container(
+          decoration: kMainContainerDecoration,
+          height: 900,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width / 12.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      // Image(
+                      //   image: AssetImage('assets/images/BMI.png'),
+                      //   height: 100,
+                      // ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 20.0,
+                        ),
+                        child: Text(
+                          MyLocalizations.of(context).gender,
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 16.0,
-                        bottom: 16.0,
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 16.0,
+                          bottom: 16.0,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Expanded(
+                              child: _buildGenderToggleButtons(
+                                title: MyLocalizations.of(context).male,
+                                gender: Gender.Male,
+                              ),
+                            ),
+                            const SizedBox(width: 16.0),
+                            Expanded(
+                              child: _buildGenderToggleButtons(
+                                title: MyLocalizations.of(context).female,
+                                gender: Gender.Female,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Expanded(
-                            child: _buildGenderToggleButtons(
-                              title: MyLocalizations.of(context).male,
-                              gender: Gender.Male,
-                            ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Text(
+                          MyLocalizations.of(context).height,
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20
                           ),
-                          const SizedBox(width: 16.0),
-                          Expanded(
-                            child: _buildGenderToggleButtons(
-                              title: MyLocalizations.of(context).female,
-                              gender: Gender.Female,
-                            ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 40.0,
+                          bottom: 16.0,
+                        ),
+                        child: CustomSlider(
+                          min: 120.0,
+                          max: 220.0,
+                          measurementUnit: 'cm',
+                          value: height,
+                          onChanged: !showResult
+                              ? (double newValue) {
+                            setState(() {
+                              height = newValue.round();
+                            });
+                          }
+                              : null,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Text(
+                          MyLocalizations.of(context).weight,
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 40.0,
+                          bottom: 16.0,
+                        ),
+                        child: CustomSlider(
+                          min: 40.0,
+                          max: 120.0,
+                          measurementUnit: 'kg',
+                          value: weight,
+                          onChanged: !showResult
+                              ? (double newValue) {
+                            setState(() {
+                              weight = newValue.round();
+                            });
+                          }
+                              : null,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Stack(
+                alignment: Alignment.topRight,
+                children: <Widget>[
+                  Container(
+                    height: 190.0,
+                    width: double.infinity,
+                    child: const CustomPaint(
+                      painter: CurvePainter(color: Color(0xFFB06AB3), pathNo: 3),
+                    ),
+                  ),
+                  Container(
+                    height: 220.0,
+                    width: double.infinity,
+                    child: const CustomPaint(
+                      painter: CurvePainter(color: Color(0xFFB06AB3), pathNo: 2),
+                    ),
+                  ),
+                  Container(
+                    height: 260.0,
+                    width: double.infinity,
+                    child: const CustomPaint(
+                      painter: CurvePainter(color: Colors.white, pathNo: 1),
+                    ),
+                  ),
+                  _buildBottomStackContent(),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: MediaQuery.of(context).size.width / 12.0,
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                            color: kPrimaryColor.withOpacity(.4),
+                            blurRadius: 6.0,
+                            offset: const Offset(0.0, 2.0),
                           ),
                         ],
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10.0),
-                      child: Text(
-                        MyLocalizations.of(context).height,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20
+                      child: FlatButton(
+                        color: Color(0xFFEECDA3),
+                        onPressed: () {
+                          setState(() {
+                            showResult = !showResult;
+                          });
+                        },
+                        padding: const EdgeInsets.all(24.0),
+                        shape: const CircleBorder(),
+                        textColor: kPrimaryColor,
+                        child: Icon(
+                          !showResult ? Icons.forward_rounded : Icons.backspace,
+                          size: 48.0,
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 40.0,
-                        bottom: 16.0,
-                      ),
-                      child: CustomSlider(
-                        min: 120.0,
-                        max: 220.0,
-                        measurementUnit: 'cm',
-                        value: height,
-                        onChanged: !showResult
-                            ? (double newValue) {
-                                setState(() {
-                                  height = newValue.round();
-                                });
-                              }
-                            : null,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10.0),
-                      child: Text(
-                        MyLocalizations.of(context).weight,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 40.0,
-                        bottom: 16.0,
-                      ),
-                      child: CustomSlider(
-                        min: 40.0,
-                        max: 120.0,
-                        measurementUnit: 'kg',
-                        value: weight,
-                        onChanged: !showResult
-                            ? (double newValue) {
-                                setState(() {
-                                  weight = newValue.round();
-                                });
-                              }
-                            : null,
-                      ),
-                    ),
-                  ],
-                ),
+                  )
+                ],
               ),
-            ),
-            Stack(
-              alignment: Alignment.topRight,
-              children: <Widget>[
-                Container(
-                  height: 190.0,
-                  width: double.infinity,
-                  child: const CustomPaint(
-                    painter: CurvePainter(color: Color(0xFFB06AB3), pathNo: 3),
-                  ),
-                ),
-                Container(
-                  height: 220.0,
-                  width: double.infinity,
-                  child: const CustomPaint(
-                    painter: CurvePainter(color: Color(0xFFB06AB3), pathNo: 2),
-                  ),
-                ),
-                Container(
-                  height: 260.0,
-                  width: double.infinity,
-                  child: const CustomPaint(
-                    painter: CurvePainter(color: Colors.white, pathNo: 1),
-                  ),
-                ),
-                _buildBottomStackContent(),
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: MediaQuery.of(context).size.width / 12.0,
-                  ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      boxShadow: <BoxShadow>[
-                        BoxShadow(
-                          color: kPrimaryColor.withOpacity(.4),
-                          blurRadius: 6.0,
-                          offset: const Offset(0.0, 2.0),
-                        ),
-                      ],
-                    ),
-                    child: FlatButton(
-                      color: Color(0xFFEECDA3),
-                      onPressed: () {
-                        setState(() {
-                          showResult = !showResult;
-                        });
-                      },
-                      padding: const EdgeInsets.all(24.0),
-                      shape: const CircleBorder(),
-                      textColor: kPrimaryColor,
-                      child: Icon(
-                        !showResult ? Icons.forward_rounded : Icons.backspace,
-                        size: 48.0,
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
+            ],
+          ),
+        )
       );
+
     }
 
     return Scaffold(
